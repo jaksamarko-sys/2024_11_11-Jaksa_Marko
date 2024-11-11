6bef2c4677b0039afad2956424be742f2681788b
jaksamarko-sys <jaksa.marko@diak.szbi-pg.hu>
2024-11-11 09:48:37 +0100

    add example files
    sssshhhh

---
+++ 1_1_feladat.py
@@ -0,0 +1,6 @@
+    """Készíts egy programot, amely a felhasználótól keresztneveket kér be egészen addig, amíg az ENTER-t nem üt (nem ad meg újabb nevet a bekérésnél)! A program a bekért neveket írja ki a képernyőre!"""
+
+
+folytat = true
+while folytat:
+    nev = input()
