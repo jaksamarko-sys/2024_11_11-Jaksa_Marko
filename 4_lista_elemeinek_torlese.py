469647e85671463a36f43b3ed65e44ba654e680c
jaksamarko-sys <jaksa.marko@diak.szbi-pg.hu>
2024-11-18 08:49:55 +0100

    add listaktorlese

---
+++ 4_lista_elemeinek_torlese.py
@@ -0,0 +1,24 @@
+nyelvek = ['Python', 'C', 'C++', 'Java', 'Python']
+   
+# eltávolítja a legutolsó elemet, és azzal tér vissza
+# itt például a törölt értéket a 'torolt_nyelv' fogja tartalmazni
+nyelvek.pop()
+print(nyelvek)
+torolt_nyelv = nyelvek.pop()
+print(f'Törölt nyelv {torolt_nyelv}')
+nyelvek.pop(1)
+# eltávolítja a megadott indexű elemet,  és azzal tér vissza
+
+
+# eltávolítja a megadott indexű elemet
+del nyelvek[1]
+print(nyelvek)
+
+# eltávolítja a megadott elemet az elsőként megtalált pozícióból
+nyelvek.remove('Python')
+print(nyelvek)
+
+# törli a listát
+nyelvek.clear()
+print(nyelvek)
+  
